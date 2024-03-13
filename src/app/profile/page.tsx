a69422b8f2422ac9e2f7c83a185fc1edf5b97ce4
= mariafadilla15 <= mariafadilla15@gmail.com>
2024-03-13 23:51:32 +0700

    update: add image to profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,36 @@
+const baseUrl = 'https://i.imgur.com/';
+const biodata = {
+    imageId: '5Q2oUMg',
+    imageSize: 'b',
+    nama: "Maria Fadilla",
+    jurusan: "Teknologi Informasi",
+    minat: "Seni dan Teknologi",
+    kontak: "mariafadilla15@gmail.com",
+    headingStyle: {
+        backgroundColor: 'lightGrey',
+        color: 'navy'
+    },
+};
+
 export default function Profile() {
-    const biodata = {
-        nama: "Maria Fadilla",
-        jurusan: "Teknologi Informasi",
-        minat: "Seni dan Teknologi",
-        kontak: "mariafadilla15@gmail.com",
-    };
+
+    const { headingStyle } = biodata;
 
     return (
         <div>
-            <h1>Welcome to My Profile</h1>
-            <p>Nama    : {biodata.nama}</p>
-            <p>Jurusan : {biodata.jurusan}</p>
-            <p>Minat   : {biodata.minat}</p>
-            <p>Kontak  : {biodata.kontak}</p>
+            <div style={headingStyle}>
+                <h1>Welcome to My Profile</h1>
+            </div>
+            <div>
+                <img
+                    className="avatar"
+                    src={`${baseUrl}${biodata.imageId}${biodata.imageSize}.jpeg`}
+                />
+                <p>Nama    : {biodata.nama}</p>
+                <p>Jurusan : {biodata.jurusan}</p>
+                <p>Minat   : {biodata.minat}</p>
+                <p>Kontak  : {biodata.kontak}</p>
+            </div>
         </div>
     );
 }
